2fcd9fb4a27860ef5e7160751721ad62def37faa
Ryan Senne <50930199+rsenne@users.noreply.github.com>
2023-06-06 13:05:57 -0400

    Update onephoton.py

--- onephoton/onephoton.py
+++ onephoton/onephoton.py
@@ -18,7 +18,7 @@ class InscopixProcessing:
         df = pd.read_csv(self.filename, header=[0, 1], index_col=0)
         # accepted needs a space because these files were saved poorly
         accepted_cells = df.xs(" accepted", axis=1, level=1)
-        rejected_cells = df.xs(" accepted", axis=1, level=1)
+        rejected_cells = df.xs(" rejected", axis=1, level=1)
         self.accepted = accepted_cells
         self.rejected = rejected_cells
         self.all_cells = df
