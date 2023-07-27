f9e474fca384bb9b235d69f01e6b8478253fe0f7
Amy Monasterio <amonast@bu.edu>
2023-07-27 13:31:55 -0400

    Update InscopixProcessing with method to get stable cell indices and registered cells

--- onephoton/onephoton.py
+++ onephoton/onephoton.py
@@ -1,5 +1,5 @@
-import tkinter.filedialog
-
+#%%
+from cell_registration import CellReg
 import numpy as np
 import pandas as pd
 import scipy.stats as stats
@@ -9,13 +9,14 @@ import os
 
 __all__ = ["InscopixProcessing"]
 
-
+#%%
 class InscopixProcessing():
-    def __init__(self, filename, data_directory = None):
+    def __init__(self, filename, animal, data_directory = None):
         self.filename = filename
         self.all_cells = None
         self.rejected = None
         self.accepted = None
+        self.animal = animal
 
         if data_directory is not None:
             self.CellReg_path = os.path.join(data_directory,'CellReg')
@@ -29,9 +30,24 @@ class InscopixProcessing():
         self.rejected = rejected_cells
         self.all_cells = df
 
+    def get_registered_cells(self,stable_all=False):
+        FOV = 'FOV1'
+        reg = CellReg(self.animal,fov=FOV,N_sessions=5,session_inds=None)
+        inds_all = reg.load_registration_table()
+        inds = inds_all.iloc[:,0:5].copy().astype(int)
+        self.reg_inds = inds
+        self.stable_inds = self.reg_inds.loc[(inds[0]!=-1) & (inds[1]!=-1) & (inds[2]!=-1) & (inds[3]!=-1) & (inds[4]!=-1)].copy()
+
+        if stable_all:
+            return self.stable_inds
+        else:
+            return self.reg_inds
+    
     def classify_cells(self):
         pass
 
     def event_triggered_average(self):
         pass
 
+
+# %%
