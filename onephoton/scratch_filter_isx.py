e58170bd9cb91ed72ee881b5077a50a4940eaf79
Amy Monasterio <amonast@bu.edu>
2023-08-15 16:08:02 -0400

    scratch for filtering accepteds from registration table

--- onephoton/scratch_filter_isx.py
+++ onephoton/scratch_filter_isx.py
@@ -2,8 +2,6 @@
 import pandas as pd 
 from cell_registration import CellReg
 from onephoton import InscopixProcessing
-cellreg = CellReg('astro5','FOV1',N_sessions=5)
-reg_ind = cellreg.get_reg_ind()
 
 data_dir = '/Users/amonast/Desktop/dCA1_astro'
 animal = 'astro5'
@@ -11,27 +9,7 @@ session= 'fc'
 isx = InscopixProcessing(session,animal,data_directory=data_dir)
 isx.read_inscopix()
 
+reg = CellReg(animal,fov='FOV1',N_sessions=2,session_inds=[0,1])
+reg_ind = reg.load_registration_table()
 
-#%% 
-def filter_all_isx:
-    if session_inds is None:
-            n_sessions = 5
-        ## for session in range(n_sessions):
 # %%
-def filter_session(traces,cell_class=='accepted')
-    
-    
-
-    if cell_class=='accepted':
-
-    elif cell_class=='rejected':
-        pass
-
-
-
-    #%%
-
-    ### test it out 
-    ## plot one cell roi next to its trace before filter
-    ## print filtered indices
-    ## plot the same cell after filtering 
