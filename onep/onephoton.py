5b42f31cce03addea2147207d14201f3962047e3
Amy Monasterio <amonast@bu.edu>
2023-08-22 09:32:44 -0400

    fixed read_inscopix from merge

--- onep/onephoton.py
+++ onep/onephoton.py
@@ -29,24 +29,8 @@ class InscopixProcessing():
         self.filename = os.path.join(self.Traces_path,animal+'_'+session+'_traces.csv')
         self.all_traces = None
         self.accepted_traces = None
-        self.rejected_traes = None
-        
-        
-    def read_inscopix(self):
-        '''
-        Reads Inscopix trace csv file. 
-        '''
-        try:
-            df = pd.read_csv(self.filename, header=[0,1], index_col=0)
-        except FileNotFoundError:
-            raise Exception('traces csv file not found, check folder structure')
-        self.all_traces = df
-        
-        # accepted vs rejected traces
-        self.filename = os.path.join(self.Traces_path, animal + '_' + session + '_traces.csv')
-        self.all_cells = None
-        self.accepted_cells = None
-        self.rejected_cells = None
+        self.rejected_traces = None
+
         
     def read_inscopix(self):
         '''
