27a3cca65a27ffab895f3aa8fa06d3ab69bb5ecf
Rebecca Suthard <suthardr@bu.edu>
2023-09-04 09:49:26 -0400

    rebecca makes a branch
    added analysis file

--- onep/onephoton.py
+++ onep/onephoton.py
@@ -4,40 +4,42 @@ import pandas as pd
 import scipy.stats as stats
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
-import os 
-from cell_registration import CellReg
+from pathlib import Path
+from onep.cell_registration import CellReg
+from onep.behavior_analysis import calculate_binned_freezing, create_freeze_vector
 __all__ = ["InscopixProcessing"]
 
-#%%
+
 class InscopixProcessing():
-    def __init__(self, session, animal, data_directory):
+    def __init__(self, animal, session, group, directory):
         '''
-        animal: string, must match in all filenames
-        session: string, must match according to mouse group + filenames (i.e. 'fc','ext1','gen1')
-        data_directory: home directory for experiment; contains subfolders for Cell_Traces, CellReg
+        directory: data folder (i.e. dCA1_Analysis)
+        animal: string, sub-folder for each astro that matches filename (i.e. 'astro3')
+        cellreg: file within animal folder (i.e. astro3_cell_reg.csv)
+        session: string, sub-folder within animal folder (i.e. 'fc','ext1','gen1')
+        traces: file within session folder (i.e. astro3_fc_traces.csv)
+        behavior: file within session folder (i.e. astro3_fc_behavior.csv)
         '''
-        self.CellReg_path = os.path.join(data_directory,'CellReg')
-        self.Traces_path = os.path.join(data_directory,'Cell_Traces',animal+'_traces')
-        
-        if not os.path.exists(self.CellReg_path):
-            raise FileNotFoundError("Couldnt find CellReg subfolder, check data directory & subfolders")
-        if not os.path.exists(self.Traces_path):
-            raise FileNotFoundError("Couldnt find Traces subfolder, check data directory & subfolders")
-        
-        self.session=session
-        self.animal = animal
-        self.filename = os.path.join(self.Traces_path,animal+'_'+session+'_traces.csv')
+        self.directory = Path(directory)
+        self.animal = self.directory/animal
+        self.cellreg = self.animal/f"{animal}_cell_reg.csv"
+        self.session = self.animal/session
+        self.traces = self.session/f"{animal}_{session}_traces.csv"
+        self.behavior = self.session/f"{animal}_{session}_behavior.csv"
+        self.group = group
+        self.timestamps = None
+        self.freeze_vector = None
+        self.binned_freezing = None
         self.all_traces = None
         self.accepted_traces = None
         self.rejected_traces = None
 
-        
     def read_inscopix(self):
         '''
         Reads Inscopix trace csv file. 
         '''
         try:
-            df = pd.read_csv(self.filename, header=[0,1], index_col=0)
+            df = pd.read_csv(self.traces, header=[0,1], index_col=0)
         except FileNotFoundError:
             raise Exception('traces csv file not found, check folder structure')
         self.all_traces = df
@@ -48,9 +50,10 @@ class InscopixProcessing():
         rejected_traces = df.xs(" rejected", axis=1, level=1) 
         self.accepted_traces = accepted_traces
         self.rejected_traces = rejected_traces
+        self.timestamps = pd.Series(df.index.to_numpy())
 
         #accepted vs rejected cell indices 
-        self.accepted_df= pd.read_csv(self.filename,header=None,index_col=0).iloc[1].map({' accepted':True,' rejected':False}).rename('cell_status').reset_index(drop=True).reset_index()
+        self.accepted_df= pd.read_csv(self.traces,header=None,index_col=0).iloc[1].map({' accepted':True,' rejected':False}).rename('cell_status').reset_index(drop=True).reset_index()
         self.accepted_inds=self.accepted_df['index'].loc[self.accepted_df['cell_status']==True].values
         self.rejected_inds=self.accepted_df['index'].loc[self.accepted_df['cell_status']==False].values
         
@@ -71,21 +74,21 @@ class InscopixProcessing():
     
     def load_registration_table(self,filter_accepted=True,session_subset=None):
         '''
-        session_subset: (optional) list, session indices (i.e. 0 for fc, 1 for ext1/gen1 etc)
+        session_subset: (optional) list, session indices (i.e. 0 for fc, 1 for recall, etc.)
         to do filter accepted
         '''
         if session_subset is not None:
-            table = CellReg(self.animal,'FOV1').load_registration_table()
+            table = CellReg(self.animal, 'FOV1').load_registration_table()
         else:
-            table = CellReg(self.animal,'FOV1',N_sessions=session_subset).load_registration_table()
-
+            table = CellReg(self.animal, 'FOV1', N_sessions=session_subset).load_registration_table()
         return table
 
-    def classify_cells(self):
-        pass
+    def behavior_analysis(self):
+        '''
+        Grabs behavioral file for a certain animal + session
+        Calculates binned_freezing and creates freeze_vector from the behavioral_analysis.py file
+        '''
+        self.binned_freezing, anymaze_df = calculate_binned_freezing(self.behavior)
+        self.freeze_vector = create_freeze_vector(anymaze_df, self.timestamps)
+        return
 
-    def event_triggered_average(self):
-        pass
-
-
-# %%
